0e89d899d9ff0f27486d608de1c88cce9e5bbeb7
ULADZISLAVA <vladislava96.96@mail.ru>
2022-11-03 20:09:37 +0300

    feat: implement 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+fs.writeFile(
+  path.join(__dirname, 'text.txt'),
+  '',
+  err => {
+    if (err) throw err;
+  }
+)
+
+stdout.write("Пожалуйста, введите текст и нажмите кнопку Enter\n");
+
+stdin.on('data', data => {
+
+  const dataStr = data.toString().trim();
+
+  if (dataStr == "exit") {
+    process.exit();
+  }
+
+  fs.appendFile(
+    path.join(__dirname, 'text.txt'),
+    data,
+    err => {
+      if (err) throw err;
+    }
+  )
+
+}) 
+
+process.on('exit', () => stdout.write('Удачи в изучении Node.js!'));
