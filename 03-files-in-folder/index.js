cb3db28e606e5683d22db6231c8ec2965d01a413
ULADZISLAVA <vladislava96.96@mail.ru>
2022-11-04 18:12:37 +0300

    feat: implement 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+
+fs.readdir(
+  path.join(__dirname, 'secret-folder'),
+  {withFileTypes: true},
+  (err, data) => {
+
+    if (err) throw err;
+    let newData = [];
+  
+    for (let fileData of data) {
+      if(fileData.isFile()) {
+        newData.push(fileData)
+      }
+    }
+
+    for (let fileData of newData) {
+
+      let extension = path.extname(fileData.name);
+      let name = path.basename(`./03-files-in-folder/secret-folder/${fileData.name}`, extension);
+      
+      fs.stat(
+        `./03-files-in-folder/secret-folder/${fileData.name}`, 
+        (err, stats) => {
+          let size = stats.size/100;
+          extension = extension.replace('.', '');
+          console.log(`${name} - ${extension} - ${size}kb`)
+        }
+      )
+    }
+  }
+)
+
+
